9551d23d60062cae617fbd01a64b5e24ab011e5d
appkr <juwonkim@me.com>
2016-02-20 12:14:23 +0900

    simplified request added

--- src/client.php
+++ src/client.php
@@ -5,3 +5,6 @@ require __DIR__ . '/../vendor/autoload.php';
 $credentials = include(__DIR__ . '/./config.php');
 echo (new App\Github)->user($credentials);
 
+//echo (new GuzzleHttp\Client([
+//    'base_uri' => 'https://api.github.com'
+//]))->get('users/' . $credentials['username'])->getBody();
